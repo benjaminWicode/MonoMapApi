0cb2e8fecab2400b4ae733420c0c4e21aac9136d
luisbenjaminhortalopez <lbhortaoopez@gmail.com>
2024-09-20 18:24:30 -0600

    MONGODB

--- src/config/env.plugin.ts
+++ src/config/env.plugin.ts
@@ -3,7 +3,7 @@ import * as env from "env-var";
 
 export const envs = {
     PORT: env.get("PORT").required().asPortNumber(),
-    MONGO_URL: env.get("MONGO_URL").required().asString(),
+    MONGO_URL: env.get("MONGO_URL_DOCKER").required().asString(),
     MAIL_SECRET_KEY: env.get("MAIL_SECRET_KEY").required().asString(),
     MAIL_SERVICE: env.get("MAIL_SERVICE").required().asString(),
     MAIL_USER: env.get("MAIL_USER").required().asString(),
